de6e4f9cbf0e2775fabf34a85b0249d0e490a4cb
Edwin Valdes <edwin.valdes2026@gmail.com>
2026-06-02 04:37:22 -0500

    Actualizacion de colores

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-function App() {
-  const [count, setCount] = useState(0)
+import React from 'react'
 
+function App() {
   return (
-    <>
+    <div className="container section-padding">
+      <h1>🧁 Bienvenidos a Ferchys Postres</h1>
+      <p>El lugar donde nacen los antojos más dulces.</p>
       
-    </>
+      <div style={{ marginTop: '2rem' }}>
+        <button className="btn btn-primario" style={{ marginRight: '1rem' }}>
+          Ver Catálogo
+        </button>
+        <button className="btn btn-secundario">
+          Hacer Pedido
+        </button>
+      </div>
+    </div>
   )
 }
 
