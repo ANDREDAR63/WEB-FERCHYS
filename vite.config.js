a6e985972e73e9ca20f0ebd00dfa148808945de8
ANDREDAR63 <andred.ar63@gmail.com>
2026-06-10 20:31:30 -0500

    Add base path to Vite configuration
    Set base path for Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  
-});
+  base: '/WEB-FERCHYS/', // Esto es lo que le dice a Vite dónde buscar los archivos
+})
