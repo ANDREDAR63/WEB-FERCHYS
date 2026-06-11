eeb0e0915a055bcce7fe8362f46fbc7e7b47ab98
ANDREDAR63 <andred.ar63@gmail.com>
2026-06-10 21:28:10 -0500

    hero fixed

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // 1. Importamos BrowserRouter
 import Layout from './components/layout/layout';
 import Hero from './components/hero/hero';
 import Catalogo from './components/catalogo/catalogo';
@@ -8,14 +8,17 @@ import Contacto from './components/contacto/contacto';
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/catalogo" element={<Catalogo />} />
-        <Route path="/nosotros" element={<About />} />
-        <Route path="/contacto" element={<Contacto />} />
-      </Routes>
-    </Layout>
+    // 2. Envolvemos todo en BrowserRouter y le pasamos el BASE_URL de Vite
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/nosotros" element={<About />} />
+          <Route path="/contacto" element={<Contacto />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
