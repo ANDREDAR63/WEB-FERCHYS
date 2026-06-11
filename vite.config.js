eb104fce2226e1943436d1093be45d4779454606
ANDREDAR63 <andred.ar63@gmail.com>
2026-06-10 20:56:02 -0500

    Configurar despliegue, añadir build y base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/WEB-FERCHYS/', // Esto es lo que le dice a Vite dónde buscar los archivos
+  base: '/WEB-FERCHYS/',
 })
