352c47dcc993960a479d3d00b5dbd7d06019c9f6
ANDREDAR63 <andred.ar63@gmail.com>
2026-08-08 10:18:51 -0500

    fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,8 @@ import About from './components/about/about';
 import Contacto from './components/contacto/contacto';
 import Login from './components/login/login'; 
 import Registration from './components/registration/registration';
-import PasswordRecovery from './components/PasswordRecovery/PasswordRecovery'; // <-- Importa el componente de recuperación de contraseña
+import PasswordRecovery from './components/PasswordRecovery/PasswordRecovery'; 
+import ShoppingCart from './components/shopping_cart/shopping_cart';
 
 function App() {
   return (
@@ -20,7 +21,8 @@ function App() {
         <Route path="/login" element={<Login />} /> 
         <Route path="/registration" element={<Registration />} />
         <Route path="/PasswordRecovery" element={<PasswordRecovery />} />
-        <Route path="/recuperar-contraseña" element={<PasswordRecovery />} /> {/* Ruta para la recuperación de contraseña */}
+        <Route path="/recuperar-contraseña" element={<PasswordRecovery />} /> 
+        <Route path="/carrito" element={<ShoppingCart />} />
       </Routes>
     </Layout>
   );
