58e7738b5487a07a4ce237ea4f7611f6538414b4
ANDREDAR63 <andred.ar63@gmail.com>
2026-08-08 08:49:21 -0500

    Fix: ajustar App.jsx para permitir build exitoso

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom'; // <-- Asegúrate de que NO tenga BrowserRouter aquí
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/layout';
 import Hero from './components/hero/hero';
 import Catalogo from './components/catalogo/catalogo';
 import About from './components/about/about';
 import Contacto from './components/contacto/contacto';
-import Login from './components/login/login'; 
-import Registration from './components/registration/registration';
-import PasswordRecovery from './components/PasswordRecovery/PasswordRecovery'; // <-- Importa el componente de recuperación de contraseña
+//import Login from './components/login/login'; 
+//import Registration from './components/registration/registration';
+//import PasswordRecovery from './components/PasswordRecovery/PasswordRecovery'; 
 
 function App() {
   return (
@@ -17,10 +17,10 @@ function App() {
         <Route path="/catalogo" element={<Catalogo />} />
         <Route path="/nosotros" element={<About />} />
         <Route path="/contacto" element={<Contacto />} />
-        <Route path="/login" element={<Login />} /> 
-        <Route path="/registration" element={<Registration />} />
-        <Route path="/PasswordRecovery" element={<PasswordRecovery />} />
-        <Route path="/recuperar-contraseña" element={<PasswordRecovery />} /> {/* Ruta para la recuperación de contraseña */}
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/registration" element={<Registration />} /> */}
+        {/* <Route path="/PasswordRecovery" element={<PasswordRecovery />} /> */}
+        {/* <Route path="/recuperar-contraseña" element={<PasswordRecovery />} /> */}
       </Routes>
     </Layout>
   );
